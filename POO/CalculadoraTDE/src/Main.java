b01d47aff5a220584dccf0869b3ac125186418d6
Vinicius Raupp <101226503+Viniciusrcarlos@users.noreply.github.com>
2023-08-27 14:41:14 -0300

    l

--- POO/CalculadoraTDE/src/Main.java
+++ POO/CalculadoraTDE/src/Main.java
@@ -3,8 +3,8 @@ import java.util.ArrayList;
 public class Main {
     public static void main(String[] args) {
         ArrayList<Double> lista = new ArrayList();
-        lista.add(20.0);
-        lista.add(30.0);
+        lista.add(50.0);
+        lista.add(50.0);
 
         Soma soma = new Soma();
         Subtracao subtracao = new Subtracao();
